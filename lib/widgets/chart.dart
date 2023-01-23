01d799c2be8b6bd09622ab7164fdfd4b44f362eb
תמר גלב <tdg400090@gmail.com>
2023-01-24 01:17:35 +0200

    All changes

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/material.dart';
+// class Chart extends StatefulWidget {
+//   const Chart({Key? key}) : super(key: key);
+//
+//   @override
+//   State<Chart> createState() => _ChartState();
+// }
+//
+// class _ChartState extends State<Chart> {
+//   @override
+//   Widget build(BuildContext context) {
+//     List instruments<Insrument> = [];
+//     return const Placeholder();
+//   }
+// }
+// class Insrument{
+//   final String hebName;
+//   final String key;
+//   final String key;
+//
+// }
